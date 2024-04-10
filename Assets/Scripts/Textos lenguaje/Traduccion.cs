f8b6580c7702c6b76c521062c95a1ab8d0b6e1d3
Ofijo79 <oriolfijo2004@gmail.com>
2024-04-10 12:48:09 +0200

    AAAAAAAAAAAAAAAAAAA

---
+++ Assets/Scripts/Textos lenguaje/Traduccion.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TextoEjercicio", menuName = "Traduccion")]
+public class Traduccion : ScriptableObject
+{
+    public string texto1;
+    public string texto2;
+    public string texto3;
+    public string texto4;
+}
